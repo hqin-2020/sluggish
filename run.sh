7ae097824cc9da5a1fae3132101c6dea03d79077
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-26 23:04:52 -0600

    new commit

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ python_name="run.py"
 maxiter=50000
 
 rhoarray=(1.00001)
-gammaarray=(8.0)
+gammaarray=(8.0 5.0 3.0 1.00001)
 # kappaarray=(0.0)
 kappaarray=(-10.0 -5.0 -3.0 -2.0 -1.0 0.0)
 
@@ -24,7 +24,7 @@ for epsilon in ${epsilonarray[@]}; do
                     for kappa in "${kappaarray[@]}"; do
                         count=0
 
-                        action_name="TwoCapital_mul_bc_kappa_test"
+                        action_name="TwoCapital_mul_bc_kappa_gamma"
 
                         action_name="${action_name}"
                         output="${action_name}_bc_${boundc}"
